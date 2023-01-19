c3e9b81d78e667b81052d45127055fc9df8f3676
Oloja <oloja@kali.kali>
2023-01-19 22:38:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Pages from "./pages/Pages";
+import Category from "./components/Category";
+import Search from "./components/Search";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Search />
+        <Category />
+        <Pages />
+      </div
+    </BrowserRouter>
   );
 }
 
